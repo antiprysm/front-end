d25a7edd41038895ee2c95b6c0c3bfadb73749d3
antiprysm <59176422+antiprysm@users.noreply.github.com>
2023-03-10 13:28:57 -0600

    result box

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,9 +4,17 @@ class Results extends React.Component {
 
   render() {
     return <div>
-  bags = {this.props.results.bags}
-  thinset = {this.props.results.thinset}
-  tiles = {this.props.results.tiles}
+  <div className="grid">
+    <div className="box c">
+        <div className="paragraph">
+      <p>bags = {this.props.results.bags}</p>
+      <p>thinset = {this.props.results.thinset}</p>
+      <p>tiles = {this.props.results.tiles}</p>
+        </div>
+      
+    </div>
+  </div>
+  
 </div>
   }
 }
